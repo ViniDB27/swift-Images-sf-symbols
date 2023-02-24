87526121a9813e3ff54394862e883635d13c6551
Vinicio Brejinski <viniciosdb@gmail.com>
2023-02-24 14:40:50 -0300

    Initial Commit

---
+++ imagesSFSymbols/imagesSFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  imagesSFSymbolsApp.swift
+//  imagesSFSymbols
+//
+//  Created by Vinicio Brejinski on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct imagesSFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
